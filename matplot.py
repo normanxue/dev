212d3e5a090dcc37066290d865c40ef6e4ac201e
normanxue <13771896911@163.com>
2025-04-10 15:15:27 +0800

    test

---
+++ matplot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# 创建数据
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 17, 13]
+
+# 绘制图形
+plt.plot(x, y)
+
+# 添加标题和标签
+plt.title('Simple Line Plot')
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+
+# 显示图形
+plt.show()
+print("plt test")
---
+++ numpytest.py
@@ -0,0 +1,3 @@
+import numpy as np
+X = np.array([[1.0, 2.0], [2.0, 3.0], [3.0, 4.0]])
+print(X)
