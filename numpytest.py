bd287f0f4bac1919499c5783d3c93270e9d3a96d
normanxue <13771896911@163.com>
2025-04-12 16:21:29 +0800

    test

--- numpytest.py
+++ numpytest.py
@@ -1,3 +1,7 @@
 import numpy as np
+import pandas as pd
 X = np.array([[1.0, 2.0], [2.0, 3.0], [3.0, 4.0]])
 print(X)
+df = pd.DataFrame(X, columns=['a', 'b'])
+print(df)
+# Compare this snippet from test.py:
