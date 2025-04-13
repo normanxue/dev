e1775255a210078d29714be2198e8b487fb81b13
normanxue <13771896911@163.com>
2025-04-13 21:49:12 +0800

    test

--- sys.py
+++ sys.py
@@ -1,5 +1,8 @@
 #   #! d:\users\candy\anaconda3\envs\dev\python.exe
 
-import sys
+
+import  sys
+# modify by local git 
 print("当前使用的python解释器路径:" + sys.executable)
 print("python版本:" +sys.version)
+ 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sys
-# modify by local git
-print(sys.executable)
-print(sys.version)
+# modify by local git 
+print("当前使用的python解释器路径:" + sys.executable)
+print("python版本:" +sys.version)
 print("hello world,中文 代码提交测试 暂存区测试")
 print("hello world,git  push test ")
