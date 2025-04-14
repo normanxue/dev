fdec27380d135abcc663ce8af206171668d9727d
normanxue <13771896911@163.com>
2025-04-14 22:12:46 +0800

    del token

--- getTushareData.py
+++ getTushareData.py
@@ -1,6 +1,6 @@
 import tushare as ts
 import pandas as pd 
-pro = ts.pro_api('2f363fe7754a53ed60082e0199278aa394cd63eb1cc4f116d0005469')  
+pro = ts.pro_api('')  
 
 df = pro.daily(ts_code='000001.SZ,600000.SH', start_date='20180701', end_date='20180718')
 print(df.head(5))
