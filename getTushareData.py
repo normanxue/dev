cbfc34ed0ea5c26ec5f396195146b4c28b95d7f4
normanxue <13771896911@163.com>
2025-04-21 22:23:20 +0800

    test

--- getTushareData.py
+++ getTushareData.py
@@ -1,6 +1,6 @@
 import tushare as ts
 import pandas as pd 
-pro = ts.pro_api('')  
+pro = ts.pro_api('7847c5bfbb1c51936a0670c6a9c93fd6b535f2650a39576a297a86ee')  
 
 df = pro.daily(ts_code='000001.SZ,600000.SH', start_date='20180701', end_date='20180718')
 print(df.head(5))
