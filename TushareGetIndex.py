db8c21096cbd31f6ad39326db95e747bd2b8bb82
normanxue <13771896911@163.com>
2025-04-27 16:15:43 +0800

    token

--- TushareGetIndex.py
+++ TushareGetIndex.py
@@ -1,8 +1,7 @@
 import tushare as ts
 import pandas as pd 
-
-pro = ts.pro_api('7847c5bfbb1c51936a0670c6a9c93fd6b535f2650a39576a297a86ee')  
-
+import myconfig # 读取配置文件
+pro = ts.pro_api(myconfig.api_token)  
 #提取沪深300指数2018年9月成分和权重
 df = pro.index_weight(index_code='399300.SZ', start_date='20180901', end_date='20180930')
 print(df.head(5))  
